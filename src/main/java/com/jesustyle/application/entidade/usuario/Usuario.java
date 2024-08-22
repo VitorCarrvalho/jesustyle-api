398518c489fcfddbd3c87d1696dcc36929ad1c28
Vitor Carvalho <vitor.carvalho@hsl.org.br>
2024-08-22 19:53:48 -0300

    ajustes final

--- src/main/java/com/jesustyle/application/entidade/usuario/Usuario.java
+++ src/main/java/com/jesustyle/application/entidade/usuario/Usuario.java
@@ -1,6 +1,7 @@
 package com.jesustyle.application.entidade.usuario;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
 
 import java.util.Date;
@@ -9,15 +10,23 @@ import java.util.Date;
 @Data
 public class Usuario {
 
+    @JsonProperty("Codigo")
     public int codigo;
+    @JsonProperty("Nome")
     public String nome;
+    @JsonProperty("DataNascimento")
     @JsonIgnoreProperties(ignoreUnknown = true)
     public Date dataNascimento;
+    @JsonProperty("CPF")
     @JsonIgnoreProperties(ignoreUnknown = true)
     public String cpf;
+    @JsonProperty("Email")
     public String email;
+    @JsonProperty("Telefone")
     @JsonIgnoreProperties(ignoreUnknown = true)
     public String telefone;
+    @JsonProperty("Senha")
     public String senha;
+    @JsonProperty("TipoUsuario")
     public String tipoUsuario; // ADMIN // USER
 }
